c9b53a3661511fbb8b667b1bfaac146bc961035d
soriftelecom41-dev <soriftelecom41@gmail.com>
2026-01-13 21:00:08 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 
+function goGame(page) {
+  window.location.href = page;
+}
+
+// Demo balance (later database হবে)
+if (!localStorage.getItem("balance")) {
+  localStorage.setItem("balance", 1310);
+}
+document.getElementById("balance").innerText =
+  localStorage.getItem("balance");
