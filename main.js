918ff2c18f7175217f7973556ae35ca37f2ae260
soriftelecom41-dev <soriftelecom41@gmail.com>
2026-01-13 21:11:10 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
+
+// balance init
+if (!localStorage.getItem("balance")) {
+  localStorage.setItem("balance", 1000); // starting balance
+}
+
+// show balance
+document.getElementById("balance").innerText =
+  localStorage.getItem("balance");
+
 function goGame(page) {
   window.location.href = page;
 }
-
-// Demo balance (later database হবে)
-if (!localStorage.getItem("balance")) {
-  localStorage.setItem("balance", 1310);
-}
-document.getElementById("balance").innerText =
-  localStorage.getItem("balance");
