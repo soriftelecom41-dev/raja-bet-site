33df4f30e6af944e726e7b7c9f2ea4a318e304ee
soriftelecom41-dev <soriftelecom41@gmail.com>
2026-01-21 17:52:36 +0600

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,63 +1,20 @@
-let deposits = JSON.parse(localStorage.getItem("depositRequests")) || [];
-let withdraws = JSON.parse(localStorage.getItem("withdrawRequests")) || [];
-let users = JSON.parse(localStorage.getItem("users")) || {};
-
-const depDiv = document.getElementById("deposits");
-const witDiv = document.getElementById("withdraws");
-
-function render(){
-  depDiv.innerHTML = "";
-  deposits.forEach((d,i)=>{
-    depDiv.innerHTML += `
-      <div class="item">
-        👤 ${d.user}<br>💰 ৳${d.amount}<br>
-        <button class="ok" onclick="approveDeposit(${i})">Approve</button>
-        <button class="no" onclick="rejectDeposit(${i})">Reject</button>
-      </div>
-    `;
-  });
-
-  witDiv.innerHTML = "";
-  withdraws.forEach((w,i)=>{
-    witDiv.innerHTML += `
-      <div class="item">
-        👤 ${w.user}<br>💸 ৳${w.amount}<br>📱 ${w.number}<br>
-        <button class="ok" onclick="approveWithdraw(${i})">Approve</button>
-        <button class="no" onclick="rejectWithdraw(${i})">Reject</button>
-      </div>
-    `;
-  });
+if(localStorage.getItem("admin")!=="true"){
+  location.href="admin-login.html";
 }
 
-function approveDeposit(i){
-  let d = deposits[i];
-  users[d.user] = (users[d.user] || 0) + Number(d.amount);
-  deposits.splice(i,1);
-  save();
+function add(){
+  let b = Number(localStorage.getItem("balance") || 1000);
+  let n = Number(document.getElementById("bal").value);
+  localStorage.setItem("balance", b + n);
+  alert("Balance Updated");
 }
 
-function rejectDeposit(i){
-  deposits.splice(i,1);
-  save();
+function reset(){
+  localStorage.setItem("balance",1000);
+  alert("Balance Reset");
 }
 
-function approveWithdraw(i){
-  let w = withdraws[i];
-  users[w.user] -= Number(w.amount);
-  withdraws.splice(i,1);
-  save();
+function logout(){
+  localStorage.removeItem("admin");
+  location.href="index.html";
 }
-
-function rejectWithdraw(i){
-  withdraws.splice(i,1);
-  save();
-}
-
-function save(){
-  localStorage.setItem("users", JSON.stringify(users));
-  localStorage.setItem("depositRequests", JSON.stringify(deposits));
-  localStorage.setItem("withdrawRequests", JSON.stringify(withdraws));
-  render();
-}
-
-render();
